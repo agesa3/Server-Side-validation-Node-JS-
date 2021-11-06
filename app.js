fb65b49f23bc73d5b093ee0b5a52951f69143c3b
agesa3 <collinsagesa2000@gmail.com>
2021-11-06 16:55:58 +0300

    set up the PORT

---
+++ app.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const app=express()
+const PORT=5000;
+
+//set the templating engine
+app,set('view engine','ejs')
+
+//navigation
+app.get('',(req,res)=>{
+    res.render('index')
+})
+
+app.get('/',(req,res)=>{
+    res.render('register')
+})
+
+app.listen(PORT,(req,res)=>{
+    console.log(`listening on port ${PORT}`)
+})
