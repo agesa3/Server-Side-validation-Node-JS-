67ee1f52209cabe802cfc39bae180a725a7a04d2
agesa3 <collinsagesa2000@gmail.com>
2021-11-06 17:13:45 +0300

    set up the body parser

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express=require('express');
 const app=express()
+const bodyParser=require('body-parser');
+const {check,validationResult}=require('express-validator');
+
 const PORT=5000;
 
 //set the templating engine
 app.set('view engine','ejs')
+const urlEncodedParser=bodyParser.urlencoded({extended:false})
 
 //navigation
 app.get('',(req,res)=>{
@@ -12,6 +16,10 @@ app.get('',(req,res)=>{
 
 app.get('/register',(req,res)=>{
     res.render('register')
+})
+
+app.post('/register',urlEncodedParser,(req,res)=>{
+    res.json(req.body)
 })
 
 app.listen(PORT,(req,res)=>{
